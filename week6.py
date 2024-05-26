6861a9eeb799f563be2bb9ac19d02a9a044b0464
MERLLIN YAZHINI <144616701+MERLLINYAZHINI@users.noreply.github.com>
2024-05-26 21:09:05 +0530

    Update week6.py

--- week6.py
+++ week6.py
@@ -1,142 +1,45 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
-# Load data
-msg = pd.read_csv('document.csv', names=['message', 'label'])
+# Sample Hypothetical Data
+data = pd.DataFrame({
+    'Fever': [1, 1, 1, 0, 0, 0, 1, 1, 0, 1],
+    'Cough': [1, 1, 0, 1, 0, 0, 1, 0, 1, 1],
+    'Fatigue': [1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
+    'Travel_History': [1, 1, 1, 0, 0, 0, 1, 0, 0, 1],
+    'COVID_Positive': [1, 1, 0, 1, 0, 0, 1, 0, 0, 1]
+})
 
-# Preprocess data
-msg['labelnum'] = msg.label.map({'pos': 1, 'neg': 0})
-X = msg.message
-y = msg.labelnum
+# Display the data
+st.title("Bayesian Network for COVID-19 Diagnosis")
+st.write("Sample Data:")
+st.write(data)
 
-# Split data
-def train_test_split(X, y, test_size=0.2, random_state=None):
-    n_samples = len(X)
-    test_samples = int(test_size * n_samples)
-    if random_state:
-        import random
-        random.seed(random_state)
-        indices = random.sample(range(n_samples), test_samples)
-    else:
-        import numpy as np
-        indices = np.random.choice(n_samples, test_samples, replace=False)
-    X_train = X.drop(indices)
-    X_test = X.iloc[indices]
-    y_train = y.drop(indices)
-    y_test = y.iloc[indices]
-    return X_train, X_test, y_train, y_test
+# Define the Bayesian Network structure and parameters
+# We assume conditional independence of symptoms given COVID_Positive
+# We manually calculate probabilities based on the data
+def predict_covid_positive(fever, cough, fatigue, travel_history):
+    covid_positive_prob = data[data['COVID_Positive'] == 1]['COVID_Positive'].sum() / len(data)
+    fever_given_covid_positive_prob = (data[(data['COVID_Positive'] == 1) & (data['Fever'] == fever)]['Fever'].sum() + 1) / \
+                                       (data[data['COVID_Positive'] == 1]['COVID_Positive'].sum() + 2)
+    cough_given_covid_positive_prob = (data[(data['COVID_Positive'] == 1) & (data['Cough'] == cough)]['Cough'].sum() + 1) / \
+                                       (data[data['COVID_Positive'] == 1]['COVID_Positive'].sum() + 2)
+    fatigue_given_covid_positive_prob = (data[(data['COVID_Positive'] == 1) & (data['Fatigue'] == fatigue)]['Fatigue'].sum() + 1) / \
+                                         (data[data['COVID_Positive'] == 1]['COVID_Positive'].sum() + 2)
+    travel_history_given_covid_positive_prob = (data[(data['COVID_Positive'] == 1) & (data['Travel_History'] == travel_history)]['Travel_History'].sum() + 1) / \
+                                                (data[data['COVID_Positive'] == 1]['COVID_Positive'].sum() + 2)
+    return covid_positive_prob * fever_given_covid_positive_prob * cough_given_covid_positive_prob * fatigue_given_covid_positive_prob * travel_history_given_covid_positive_prob
 
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+# Input for symptoms
+st.write("Enter symptoms to diagnose:")
+fever = st.selectbox("Fever", [1, 0], format_func=lambda x: 'Yes' if x == 1 else 'No')
+cough = st.selectbox("Cough", [1, 0], format_func=lambda x: 'Yes' if x == 1 else 'No')
+fatigue = st.selectbox("Fatigue", [1, 0], format_func=lambda x: 'Yes' if x == 1 else 'No')
+travel_history = st.selectbox("Travel History", [1, 0], format_func=lambda x: 'Yes' if x == 1 else 'No')
 
-# Vectorize text
-class CountVectorizer:
-    def __init__(self):
-        self.vocab = {}
-    
-    def fit_transform(self, X):
-        for text in X:
-            for word in text.split():
-                if word not in self.vocab:
-                    self.vocab[word] = len(self.vocab)
-        X_dm = []
-        for text in X:
-            doc_vector = [0] * len(self.vocab)
-            for word in text.split():
-                if word in self.vocab:
-                    doc_vector[self.vocab[word]] += 1
-            X_dm.append(doc_vector)
-        return X_dm
+# Predict the probability of COVID-19
+prob_covid_positive = predict_covid_positive(fever, cough, fatigue, travel_history)
 
-    def transform(self, X):
-        X_dm = []
-        for text in X:
-            doc_vector = [0] * len(self.vocab)
-            for word in text.split():
-                if word in self.vocab:
-                    doc_vector[self.vocab[word]] += 1
-            X_dm.append(doc_vector)
-        return X_dm
-
-count_v = CountVectorizer()
-Xtrain_dm = count_v.fit_transform(X_train)
-Xtest_dm = count_v.transform(X_test)
-
-# Naive Bayes classifier
-class MultinomialNB:
-    def __init__(self):
-        self.p_pos = None
-        self.p_neg = None
-    
-    def fit(self, X, y):
-        pos_count = 0
-        neg_count = 0
-        pos_freq = [0] * len(X[0])
-        neg_freq = [0] * len(X[0])
-        for i in range(len(X)):
-            if y.iloc[i] == 1:
-                pos_count += 1
-                for j in range(len(X[i])):
-                    pos_freq[j] += X[i][j]
-            else:
-                neg_count += 1
-                for j in range(len(X[i])):
-                    neg_freq[j] += X[i][j]
-        self.p_pos = [freq / sum(pos_freq) for freq in pos_freq]
-        self.p_neg = [freq / sum(neg_freq) for freq in neg_freq]
-    
-    def predict(self, X):
-        preds = []
-        for doc in X:
-            pos_prob = 1
-            neg_prob = 1
-            for i in range(len(doc)):
-                if doc[i] > 0:
-                    pos_prob *= self.p_pos[i] ** doc[i]
-                    neg_prob *= self.p_neg[i] ** doc[i]
-            pred = 1 if pos_prob > neg_prob else 0
-            preds.append(pred)
-        return preds
-
-# Train Naive Bayes classifier
-clf = MultinomialNB()
-clf.fit(Xtrain_dm, y_train)
-pred = clf.predict(Xtest_dm)
-
-# Display predictions
-st.write("Predictions:")
-for doc, p in zip(X_test, pred):
-    p = 'pos' if p == 1 else 'neg'
-    st.write(f"{doc} -> {p}")
-
-# Calculate metrics
-def accuracy_score(y_true, y_pred):
-    correct = sum(1 for true, pred in zip(y_true, y_pred) if true == pred)
-    return correct / len(y_true)
-
-def confusion_matrix(y_true, y_pred):
-    cm = [[0, 0], [0, 0]]
-    for true, pred in zip(y_true, y_pred):
-        cm[true][pred] += 1
-    return cm
-
-def precision_score(y_true, y_pred):
-    tp = sum(1 for true, pred in zip(y_true, y_pred) if true == 1 and pred == 1)
-    fp = sum(1 for true, pred in zip(y_true, y_pred) if true == 0 and pred == 1)
-    return tp / (tp + fp) if (tp + fp) > 0 else 0
-
-def recall_score(y_true, y_pred):
-    tp = sum(1 for true, pred in zip(y_true, y_pred) if true == 1 and pred == 1)
-    fn = sum(1 for true, pred in zip(y_true, y_pred) if true == 1 and pred == 0)
-    return tp / (tp + fn) if (tp + fn) > 0 else 0
-
-# Display metrics
-accuracy = accuracy_score(y_test, pred)
-recall = recall_score(y_test, pred)
-precision = precision_score(y_test, pred)
-conf_matrix = confusion_matrix(y_test, pred)
-
-st.write('Accuracy Metrics:')
-st.write('Accuracy:', accuracy)
-st.write('Recall:', recall)
-st.write('Precision:', precision)
-st.write('Confusion Matrix:', conf_matrix)
+# Display the result
+st.write("Probability of being COVID-19 positive:")
+st.write(prob_covid_positive)
